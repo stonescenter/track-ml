6f359ac056a94d56f03c5e2e994b6b368ae0ecc9
Jefferson Fialho <jfialho@ncc.unesp.br>
2019-04-17 17:24:29 -0300

    add fake track plots

--- utils/tracktop.py
+++ utils/tracktop.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import random
 import numpy as np
 
-def err_gauss(position, err, len_err = 50):
+def err_gauss(position, err = 0.1, len_err = 50):
     err_array = np.linspace(1 - err, 1 + err, len_err)
     value_err = position * random.choice(err_array)
     return value_err
