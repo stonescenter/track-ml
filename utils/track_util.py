3086ca8ba5949c9021d2dcf3fc4585e240f112d4
Jefferson Fialho <jfialho@ncc.unesp.br>
2019-04-17 15:22:44 -0300

    error function

---
+++ utils/track_util.py
@@ -0,0 +1,9 @@
+from scipy import special
+import matplotlib.pyplot as plt
+import random
+import numpy as np
+
+def err_gauss(position, err, len_err = 50):
+    err_array = np.linspace(1 - err, 1 + err, len_err)
+    value_err = position * random.choice(err_array)
+    return value_err
