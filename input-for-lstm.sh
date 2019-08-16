dc9849b0aef437e47a5f13d6b1551495e881dad7
Silvio Luiz Stanzani <silvio.stanzani@gmail.com>
2019-08-16 16:57:07 -0300

    new lstm 2

---
+++ input-for-lstm.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#1 -> file to transform
+#ex: ~/pt1p0_train_1_realv3_10
+
+x=8
+y=31
+echo "$x -- $y"
+cut $1 -d "," -f $x-$y | tail -n+2 > res/res0
+
+#for i in `seq 1 25` ; do
+for i in `seq 1 24` ; do
+  x=$(( $x + 6 ))
+  y=$(( $y + 6 ))
+  echo "$x -- $y"
+  cut $1 -d "," -f $x-$y | tail -n+2 > res/res$i
+done
+
+cut $1 -d "," -f 1-31 | head -n 1 > res/headdd
+cut $1 -d "," -f 8-31 | head -n 1 > res/head
+cut $1 -d "," -f 1-7 | head -n 1 > res/headP
+
+cut $1 -d "," -f 1-7 | tail -n+2 > res/aux
+
+cat res/head > res/total
+cat res/headP > res/totalP
+for i in `seq 0 24` ; do
+  cp res/aux res/aux$i
+  cat res/res$i >> res/total
+  cat res/aux$i >> res/totalP
+done
+
+paste -d, res/totalP res/total > res/resfinal
+#paste res/totalP res/total > res/resfinal
--- lstm-map-demo-div-v2-inf.py
+++ lstm-map-demo-div-v2-inf.py
@@ -28,6 +28,8 @@ import pandas as pd
 
 from pandas.plotting import register_matplotlib_converters
 
+import datetime
+
 import matplotlib.pyplot as plt
 register_matplotlib_converters()
 
@@ -35,14 +37,14 @@ def plot_results_inferences(df22):
     sns.distplot(df22[0], color="blue") #, ax=axes[0,0], axlabel='X-original')
     #plt.show()
 
-def position_3D_approximation(y, result):
-    # y => original 3d point
+#def position_3D_approximation(y, result):
+def position_3D_approximation(result):
     # result => predicted
+    #yclone = np.copy(y)
+    #dfyclone = pd.DataFrame.from_records(yclone)
 
-    yclone = np.copy(y)
-    dfyclone = pd.DataFrame.from_records(yclone)
+    global dfyclone
 
-    #df = pd.DataFrame({'X':result[0:10,0],'Y':result[0:10,1],'Z':result[0:10,2]})
     df3d = pd.DataFrame({'X':result[:,0],'Y':result[:,1],'Z':result[:,2]})
 
     df3d['X-pred'] = 0
@@ -52,16 +54,29 @@ def position_3D_approximation(y, result):
     df3d['ch1'] = 0
     df3d['w'] = 0
 
+    dfyclone = pd.DataFrame.from_records(yclone)
+
+    #print(datetime.datetime.now())
+
     for index, row in df3d.iterrows():
+        #print("1",datetime.datetime.now())
+        #print("index ", index)
         #obtain the row with least geometric distance between predicted row and original rows (in yclone)
         Xpred=df3d.loc[index, 'X']
         Ypred=df3d.loc[index, 'Y']
         Zpred=df3d.loc[index, 'Z']
+        #print("2",datetime.datetime.now())
 
-        dfyclone = pd.DataFrame.from_records(yclone)
-        dfyclone['geodist'] = ( abs(((dfyclone[0] - Xpred) **2) + ((dfyclone[1] - Ypred) **2)   + ((dfyclone[2] - Zpred) **2)) )
+        #dfyclone = pd.DataFrame.from_records(yclone)
+        #dfyclone = pd.DataFrame.from_records(yclone)
+        #print("shape: " , dfyclone.shape)
+
+        dfyclone['geodist'] = ( ((dfyclone[0] - Xpred) **2) + ((dfyclone[1] - Ypred) **2)   + ((dfyclone[2] - Zpred) **2) )
+        #print("3",datetime.datetime.now())
+
 
         row=dfyclone.loc[dfyclone['geodist'].idxmin()]
+        #print("4",datetime.datetime.now())
 
         lowerGeodistIndex=row.name
 
@@ -70,16 +85,23 @@ def position_3D_approximation(y, result):
         Z=yclone[lowerGeodistIndex,2]
         ch0=yclone[lowerGeodistIndex,3]
         ch1=yclone[lowerGeodistIndex,4]
+        #print("5",datetime.datetime.now())
 
         df3d.loc[index, 'X-pred'] = X
         df3d.loc[index, 'Y-pred'] = Y
         df3d.loc[index, 'Z-pred'] = Z
         df3d.loc[index, 'ch0'] = ch0
         df3d.loc[index, 'ch1'] = ch1
+        #print("6",datetime.datetime.now())
 
         #remove the hit used as approximation
-        yclone = np.delete(yclone, lowerGeodistIndex, axis=0)
+        dfyclone.drop(dfyclone.index[lowerGeodistIndex])
+        #yclone = np.delete(yclone, lowerGeodistIndex, axis=0)
+        #print("7",datetime.datetime.now())
 
+    #print(datetime.datetime.now())
+
+    #print("j ")
     df3d.drop('X', axis=1, inplace=True)
     df3d.drop('Y', axis=1, inplace=True)
     df3d.drop('Z', axis=1, inplace=True)
@@ -92,22 +114,32 @@ seed(1)
 #Read input file with first 3 hits to dataframe
 event_prefix = sys.argv[1]
 event_file_name=ntpath.basename(event_prefix)
-df = pd.read_csv(event_prefix)
+event_prefix2 = sys.argv[2]
 
+df = pd.read_csv(event_prefix)
+df_all_hits = pd.read_csv(event_prefix2)
 #dataframe with original values for evaluation
 #dfeval = pd.read_csv(event_prefix)
 
 #all tracks have at maximum 29 hits
 hits = np.arange(0,29,1)
 
+#print (hits)
 firstHit=1
 lastHit=4
 
-print ("begin")
-df1 = df.iloc[:,0:24]
-print("!!-print(df1.shape) : ",df1.shape)
+#print ("begin")
+df1 = df.iloc[:,1:25]
+#print("!!-print(df1.shape) : ",df1.shape)
 
-for firstHit in range(1, 24):
+y=df_all_hits.iloc[:, [ 25,26,27,28,29 ]]
+
+yclone = np.copy(y)
+dfyclone = pd.DataFrame.from_records(yclone)
+
+#for firstHit in range(1, 24):
+for firstHit in range(1, 26):
+#for firstHit in range(1, 2):
     #print("firstHit : ", firstHit)
     lastHit = lastHit + 1
     #begin with 3 know hits
@@ -125,8 +157,11 @@ for firstHit in range(1, 24):
     print ("Y")
     print ( (hits[hit_to_predict]*6)+1,(hits[hit_to_predict]*6)+2,(hits[hit_to_predict]*6)+3,(hits[hit_to_predict]*6)+4,(hits[hit_to_predict]*6)+5 )
     '''
-    
-    cpton=((hits[known_hits[2]]*6)+5)+1
+
+    #cpton=((hits[known_hits[2]]*6)+5)+1
+    #print("cpton ",cpton)
+    #print((hits[known_hits[0]]*6)+1,(hits[known_hits[0]]*6)+2,(hits[known_hits[0]]*6)+3 , (hits[known_hits[1]]*6)+1,(hits[known_hits[1]]*6)+2,(hits[known_hits[1]]*6)+3 , (hits[known_hits[2]]*6)+1,(hits[known_hits[2]]*6)+2,(hits[known_hits[2]]*6)+3)
+    #print((hits[known_hits[0]]*6)+4,(hits[known_hits[0]]*6)+5 , (hits[known_hits[1]]*6)+4 , (hits[known_hits[1]]*6)+5  , (hits[known_hits[2]]*6)+4 , (hits[known_hits[2]]*6)+5)
 
     dataX2=df1.iloc[:, [ (hits[known_hits[0]]*6)+1,(hits[known_hits[0]]*6)+2,(hits[known_hits[0]]*6)+3 , (hits[known_hits[1]]*6)+1,(hits[known_hits[1]]*6)+2,(hits[known_hits[1]]*6)+3 , (hits[known_hits[2]]*6)+1,(hits[known_hits[2]]*6)+2,(hits[known_hits[2]]*6)+3 ]]
     dataXfeatures=df1.iloc[:, [  (hits[known_hits[0]]*6)+4,(hits[known_hits[0]]*6)+5 , (hits[known_hits[1]]*6)+4 , (hits[known_hits[1]]*6)+5  , (hits[known_hits[2]]*6)+4 , (hits[known_hits[2]]*6)+5  ]]#dataXfeatures=df.iloc[:, [  hits[known_hits[0]]+4,hits[known_hits[0]]+5,hits[known_hits[0]]+6 , hits[known_hits[1]]+4,hits[known_hits[1]]+5,hits[known_hits[1]]+6 , hits[known_hits[2]]+4,hits[known_hits[2]]+5,hits[known_hits[2]]+6  ]]
@@ -140,18 +175,26 @@ for firstHit in range(1, 24):
     #Xfeat = np.reshape(b,(n_patterns,3,3))
 
     #original hit that will be predicted
-    y=df.iloc[:, [ (hits[hit_to_predict]*6)+1,(hits[hit_to_predict]*6)+2,(hits[hit_to_predict]*6)+3,(hits[hit_to_predict]*6)+4,(hits[hit_to_predict]*6)+5 ]]
+    #yaux=df22.iloc[:, [ (hits[hit_to_predict]*6)+1,(hits[hit_to_predict]*6)+2,(hits[hit_to_predict]*6)+3,(hits[hit_to_predict]*6)+4,(hits[hit_to_predict]*6)+5 ]]
+    #y=df.iloc[:, 25,26,27,28,29 ]]
 
     #perform the prediction
-    model = load_model('model.h5')
-    result = model.predict([X, Xfeat])
-    pred = position_3D_approximation(y, result)
+    model = load_model('/home/silvio/model.h5')
 
+    result = model.predict([X, Xfeat],verbose=1)
+    #print("position 3d")
+    #pred = position_3D_approximation(y, result)
+    pred = position_3D_approximation(result)
+    #print("position 3d 2")
     #concat tracks with predicted positions
     df1= pd.concat([df1,pred],axis=1) #, keys=['One','Two'])
 
+df1.to_csv('reconstructed_track.csv')
+
+
+'''
     # evaluation
-    dfglobal_original = np.hstack((dataX2,y))
+    dfglobal_original = np.hstack((dataX2,yaux))
     dfglobal_predicted = np.hstack((dataX2,pred))
 
     dftot = pd.DataFrame.from_records(dfglobal_predicted)
@@ -179,9 +222,9 @@ for firstHit in range(1, 24):
         #print('equal 0: ', dftotfinal[dftotfinal[0] == 0].sum()  )
 
     plot_results_inferences(dftotfinal)
+'''
 
-
-    '''
+'''
     print('===========================================')
     print('X')
     print(X)
@@ -192,9 +235,9 @@ for firstHit in range(1, 24):
     print('y')
     print(y)
     print('===========================================')
-    '''
+'''
 
-    '''
+'''
     print('===========================================')
     print('dfglobal_original')
     print(dfglobal_original)
@@ -202,7 +245,7 @@ for firstHit in range(1, 24):
     print('dfglobal_predicted')
     print(dfglobal_predicted)
     print('===========================================')
-    '''
+'''
 
     #dftotfinal['Xdiff'] = pd.DataFrame(abs(dftot[9]) - abs(dftot2[9])) + (abs(dftot[10]) - abs(dftot2[10])) + (abs(dftot[11]) - abs(dftot2[11]))
     #dftotfinal = pd.DataFrame( (abs((dftot[9] - dftot2[9]))) + (abs((dftot[10] - dftot2[10]))) + (abs(dftot[11]) - abs(dftot2[11])) )
